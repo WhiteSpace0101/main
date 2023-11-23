519a7c010a3940f95c044741af2abd0892a9db77
DaneR <drandolph746@gmail.com>
2023-11-23 09:12:50 -0800

    First Push

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Insert code here" << std::endl;
+    std::cout << "File name is 'main' so make sure to run code as './main'." << std::endl;
+    return 0;
+}
